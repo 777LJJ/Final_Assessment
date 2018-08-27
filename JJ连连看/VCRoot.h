72db2b7a3dc2f2c2c756f3951e956608c0d5e73a
777LJJ <1224779231@qq.com>
2018-08-27 11:45:35 +0800

    实现连连的基本功能，没有完成适配

---
+++ JJ连连看/VCRoot.h
@@ -0,0 +1,18 @@
+//
+//  VCRoot.h
+//  JJ连连看
+//
+//  Created by J J on 2018/8/27.
+//  Copyright © 2018年 J J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRoot : UIViewController
+
+@property(retain,nonatomic)UIButton *button;
+@property(retain,nonatomic)UILabel *label;
+@property(retain,nonatomic)UILabel *label01;
+@property(retain,nonatomic)UIImageView *imageView;
+
+@end
